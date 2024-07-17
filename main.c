35d4db7b8f8c4a23ec5b6d76ce213622a64a91d6
Zafer Balkan <zafer@zaferbalkan.com>
2024-07-17 20:55:45 +0300

    Monor refactor to demo application.

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include <string.h>
 #include "FlexAlloc.h"
 
+// Constants for initial settings
+#define INITIAL_CAPACITY 4
+#define ALIGNMENT 16
+#define COMMAND_SIZE 10
+
 // Dynamic array structure
 typedef struct {
     int* data;
@@ -20,6 +25,10 @@ static void initArray(DynamicArray* array, size_t initial_capacity, size_t align
     memory_block block = allocate(initial_capacity * sizeof(int), alignment);
     if (block.ptr != NULL) {
         array->data = (int*)block.ptr;
+        memset(array->data, 0, initial_capacity * sizeof(int)); // Initialize memory
+    }
+    else {
+        printf("Failed to allocate initial memory.\n");
     }
 }
 
@@ -50,6 +59,7 @@ static int resizeArray(DynamicArray* array, size_t new_capacity) {
                 array->capacity = new_capacity;
             }
             else {
+                printf("Failed to allocate memory during resize.\n");
                 return 0; // Failure
             }
         }
@@ -71,7 +81,7 @@ static int addElement(DynamicArray* array, int element) {
 }
 
 // Print the dynamic array
-static void printArray(DynamicArray* array) {
+static void printArray(const DynamicArray* array) {
     printf("Array size: %zu, capacity: %zu\n", array->size, array->capacity);
     for (size_t i = 0; i < array->size; i++) {
         printf("%d ", array->data[i]);
@@ -87,11 +97,29 @@ static int safe_strcmp(const char* s1, const char* s2) {
     return strcmp(s1, s2);
 }
 
+// Process user command
+static void processCommand(DynamicArray* array, const char* command, int value) {
+    if (safe_strcmp(command, "add") == 0) {
+        if (!addElement(array, value)) {
+            printf("Failed to add element %d\n", value);
+        }
+    }
+    else if (safe_strcmp(command, "print") == 0) {
+        printArray(array);
+    }
+    else if (safe_strcmp(command, "exit") == 0) {
+        // Do nothing, the main loop will handle exiting
+    }
+    else {
+        printf("Unknown command. Please try again.\n");
+    }
+}
+
 int main() {
     DynamicArray array;
-    initArray(&array, 4, 16); // Initial capacity of 4 with 16-byte alignment
+    initArray(&array, INITIAL_CAPACITY, ALIGNMENT);
 
-    char command[10];
+    char command[COMMAND_SIZE];
     int value;
 
     while (1) {
@@ -100,31 +128,26 @@ int main() {
             continue;
         }
 
+        if (safe_strcmp(command, "exit") == 0) {
+            break;
+        }
+
         if (safe_strcmp(command, "add") == 0) {
-            if (scanf("%d", &value) == 1) {
-                if (!addElement(&array, value)) {
-                    printf("Failed to add element %d\n", value);
-                }
-            }
-            else {
+            if (scanf("%d", &value) != 1) {
                 printf("Invalid value. Please enter an integer.\n");
                 // Clear the input buffer
                 int c;
                 while ((c = getchar()) != '\n' && c != EOF);
+                continue;
             }
         }
-        else if (safe_strcmp(command, "print") == 0) {
-            printArray(&array);
-        }
-        else if (safe_strcmp(command, "exit") == 0) {
-            break;
-        }
         else {
-            printf("Unknown command. Please try again.\n");
+            value = 0; // No value needed for other commands
         }
+
+        processCommand(&array, command, value);
     }
 
-    // Free the array
     freeArray(&array);
 
     return 0;
